4408bb08572374c114d71b982ce4ffabee70d620
kemo-byte <kamalkafi12@gmail.com>
2022-03-02 17:05:58 +0200

    first event with mail

---
+++ app/Http/Controllers/NewsLetterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\UserSubscribed;
+use Illuminate\Http\Request;
+
+class NewsLetterController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|unique:newsletter,email'
+        ]);
+
+        event(new UserSubscribed($request->input('email')));
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsLetterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[NewsLetterController::class,'index']);
+Route::post('/subscribe', [NewsLetterController::class,'subscribe']);
